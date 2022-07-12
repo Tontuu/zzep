43d8632594e7c0877b4a66744cac6cdca1c766e6
Tontuu <tons2.jerry@gmail.com>
2022-07-12 00:54:45 -0300

    Improve readability and implement UI struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-/*Reaction-Time Tester.
-Write a program that measures how quickly a user
-can respond. The program waits for a random
-interval of time then prints a single digit on the
-screen. The user has to type that digit as quickly as
-possible. The program records how long it takes the
-user to respond. Your program should perform 10
-such tests and report the minimum, maximum and
-average response time. (Hint, read the manual
-page for gettimeofday).*/
-
 use ncurses::*;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use rand::Rng;
 
-const READ_TEXT: &str = "Get Ready!";
-const TITLE_TEXT: &str = "Reaction-Time Game";
+const SETUP_TEXT: &str = "Reaction-Time Game";
+const READY_TEXT: &str = "Get Ready!";
 const WIN_TEXT: &str = "[ You won ]";
-const LOSE_TEXT: &str = "[ You lost ]";
+const LOSE_TEXT: &str = " [ You lost ]";
 
 #[derive(PartialEq)]
 enum GameState {
@@ -32,29 +21,63 @@ enum Result {
     Lose
 }
 
-fn setup() {
-    clear();
-    mv((LINES()-1)/2, ((COLS()-1)/2) - TITLE_TEXT.len() as i32 /2);
-    addstr(TITLE_TEXT);
+#[derive(Default, Copy, Clone)]
+struct Vec2 {
+    x: i32,
+    y: i32,
 }
 
-fn init_game() -> (Result, String) {
-    flushinp();
 
+
+#[derive(Default, Copy, Clone)]
+struct Ui {
+    pos: Vec2
+}
+
+impl Ui {
+       fn set_pos(&mut self, pos: Vec2) {
+	self.pos = pos;
+    }
+
+    fn center_pos(&mut self) {
+	self.pos.x = self.pos.x / 2;
+	self.pos.y = self.pos.y / 2;
+    }
+
+    fn offset(&mut self, x: i32, y: i32) {
+	self.pos.x += x;
+	self.pos.y += y;
+    }
+
+    fn label(&self, text: &str) {
+	mv(self.pos.y, self.pos.x);
+	addstr(text);
+    }
+}
+
+fn setup(mut ui: Ui) {
+    clear();
+    ui.offset(-(SETUP_TEXT.len() as i32 / 2), 0);
+    ui.label(SETUP_TEXT);
+}
+
+fn init_game(mut ui: Ui) -> (Result, String) {
+    noecho();
     let rand_row = rand::thread_rng().gen_range(0..LINES()-1);
     let rand_col = rand::thread_rng().gen_range(0..COLS()-1);
     let rand_ch = rand::thread_rng().gen_range(65..90) as u8 as char;
     let cooldown = rand::thread_rng().gen_range(1..5);
 
     clear();
-    mv((LINES()-1)/2, ((COLS()-1)/2)-READ_TEXT.len() as i32 /2);
-    addstr(READ_TEXT);
+    ui.offset(-(READY_TEXT.len() as i32 / 2), 0);
+    ui.label(READY_TEXT);
     refresh();
     sleep(Duration::from_secs(2));
     for i in (1..4).rev() {
 	clear();
-	mv((LINES()-1)/2, (COLS()-1)/2);
-	addstr(&i.to_string());
+	ui.set_pos(Vec2{ x:COLS()-1, y:LINES()-1 });
+	ui.center_pos();
+	ui.label(&i.to_string());
 	refresh();
 	sleep(Duration::from_secs(1));
     }
@@ -69,6 +92,9 @@ fn init_game() -> (Result, String) {
 
     let now = Instant::now();
 
+    // Throws away any typeahead that has been typed by the user at waiting time.
+    flushinp();
+
     let key = getch() as u8 as char;
 
     let mut result = Result::Lose;
@@ -80,17 +106,19 @@ fn init_game() -> (Result, String) {
 }
 
 fn main() {
-
     initscr();
-    noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
     let mut state = GameState::Setup;
+    let mut ui = Ui::default();
 
+    let pos = Vec2 { x: COLS()-1, y: LINES()-1};
+    ui.set_pos(pos);
+    ui.center_pos();
 
     while state != GameState::End {
 	if state == GameState::Setup {
-	    setup();
+	    setup(ui);
 	}
 
 	let key = getch();
@@ -103,13 +131,15 @@ fn main() {
 	}
 
 	if state == GameState::Play {
-	    let (result, time) = init_game();
+	    let (result, time) = init_game(ui);
 	    if result == Result::Win {
-		mv((LINES()-1)/2, ((COLS()-1)/2) - WIN_TEXT.len() as i32 /2);
-		addstr(WIN_TEXT);
+		ui.offset(-(WIN_TEXT.len() as i32 / 2), 0);
+		ui.label(WIN_TEXT);
+		ui.offset(WIN_TEXT.len() as i32 / 2, 0);
 	    } else {
-		mv((LINES()-1)/2, ((COLS()-1)/2) - LOSE_TEXT.len() as i32 /2);
-		addstr(LOSE_TEXT);
+		ui.offset(-(LOSE_TEXT.len() as i32 / 2), 0);
+		ui.label(LOSE_TEXT);
+		ui.offset(LOSE_TEXT.len() as i32 / 2, 0);
 	    } 
 	    mv((LINES()+2)/2, ((COLS()-1)/2) - time.len() as i32 / 2);
 	    addstr(&time);
@@ -119,3 +149,4 @@ fn main() {
     }
     endwin();
 }
+
